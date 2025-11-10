6207e3a70d2f39ff0203f90253f8e47ec6d27da5
Ketlen <ketlen.6@hotmail.com>
2025-11-10 20:40:00 -0300

    Terminando o controller

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject/Produto.java
@@ -0,0 +1,23 @@
+// Produto.java
+package com.mycompany.mavenproject;
+
+/**
+ * Classe de entidade simples que representa um Produto.
+ */
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
